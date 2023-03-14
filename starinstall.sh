633b9d2b03f99cb11cc169f22abc7d63f1042614
versacemilk <flaakmonkey@gmal.com>
2023-03-13 22:02:26 -0400

    modified:   starinstall.sh

--- starinstall.sh
+++ starinstall.sh
@@ -5,3 +5,7 @@
 dnf copr enable atim/starship -y
 dnf install starship -y
 
+curl https://github.com/versacemilk/StarshipConfig/blob/main/starinstall.sh
+mv ~/Downloads/starinstall.sh ~/.config/starship.toml
+
+echo "Done"
